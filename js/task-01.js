fdfa5dc1cc88d595afd588032fa947876cb0b58f
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-17 19:40:05 +0300

    цйу

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,25 @@
-const elemUl = document.querySelectorAll('ul')
-console.log(elemUl)
+// const allCategoriesEl = document.querySelectorAll('ul#categories li.item');
+// const numberOfCatrgoriesEl = allCategoriesEl.length;
+// console.log(`Number of categories: ${numberOfCatrgoriesEl}`);
+// console.log('')
 
-const elemLi = document.querySelectorAll('li')
-console.log(elemLi)
+// for(let i = 0; i < numberOfCatrgoriesEl; i += 1) {
+//     const headerTextEl = allCategoriesEl[i].firstElementChild.textContent;
+//     console.log(`Category: ${headerTextEl}`)
+
+//     const elementsEl = allCategoriesEl[i].lastElementChild
+//     const numberOfElementsEl = elementsEl.querySelectorAll('li')
+//     console.log(`Elements: ${numberOfElementsEl.length}`)
+//     console.log('')
+// }
+
+
+const allCategoriesEl = document.querySelectorAll('ul#categories li.item');
+const numberOfCatrgoriesEl = allCategoriesEl.length;
+console.log(`Number of categories: ${numberOfCatrgoriesEl}`);
+console.log('')
+
+const headerTextEl = allCategoriesEl.firstElementChild.textContent;
+ 
+headerTextEl.forEach(element => );
+console.log(`Category: ${headerTextEl}`)
