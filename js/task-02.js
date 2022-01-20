fc7f1715d39d2e9b7ae5154436c7599a5e03957f
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-20 16:30:15 +0300

    ЙЦУ

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,25 @@
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// const ingredientList = document.querySelector('#ingredients')
+
+// const ingredientsListItems = ingredients.map((ingredient) => {
+//   const itemLi = document.createElement('li')
+//   itemLi.textContent = ingredient
+//   itemLi.classList.add('item')
+//   console.log(itemLi)
+//   return itemLi
+// })
+
+// ulElem.append(...ingredientsListItems)
+// console.log(ingredientsListItems);
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -10,6 +32,12 @@ const ingredients = [
 const ingredientList = document.querySelector('#ingredients')
 
 const ingredientsListItems = ingredients.map((ingredient) => {
-
-  
+  const itemLi = document.createElement('li')
+  itemLi.textContent = ingredient
+  itemLi.classList.add('item')
+  console.log(itemLi)
+  return itemLi
 })
+
+console.log(ingredientsListItems);
+ingredientList.append(...ingredientsListItems)
