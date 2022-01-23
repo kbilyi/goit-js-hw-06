1f7c1a2bb820bebcd71c597c4601cfeb673f4a80
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-23 16:00:56 +0300

    цйу

--- js/task-04.js
+++ js/task-04.js
@@ -2,8 +2,10 @@ const incrementBtn = document.querySelector('[data-action="increment"]')
 const decrementBtn = document.querySelector('[data-action="decrement"]')
 const valueBtn = document.getElementById('value')
 
-incrementBtn.addEventListener('click', () => {
-    valueBtn = incrementBtn + 1
-    return valueBtn
-})
-incrementBtn()
+
+
+ 
+
+
+
+//  
