cd70f7c5257da01ca9cfbb4966f96861e2600eac
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-24 15:05:50 +0300

    йцу

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,41 @@
-const incrementBtn = document.querySelector('[data-action="increment"]')
-const decrementBtn = document.querySelector('[data-action="decrement"]')
-const valueBtn = document.getElementById('value')
+const minustBtn = document.querySelector('[data-action="increment"]')
+const plusBtn = document.querySelector('[data-action="decrement"]')
+const valueElem = document.getElementById('value')
+let count = 0
+
+
+// const plusFunction = elem => {
+//     for(const i = 0; i < count; i++) {
+//     if(document.onclick = plusBtn) {
+//         count += 1
+//         console.log(count);
+//     }
+// }
+// }
+// plusFunction()
 
 
 
- 
+// document.onclick = event => {
+//     if(onclick.plusBtn) {
+//         console.log(minustBtn);
+//         plusFunction(minustBtn)
+//     }
+//     if(event.target.classList.contains('decrement')) {
+//         plusFunction(event.target.dataset.id)
+//     }
+// }
 
+// const plusFunction = id => {
+//     for() {
+//         valueElem++
+//     }
+// }
 
+// const minusFunction = id => {
+//     valueElem--
+// }
 
-//  
+document.onclick = event => {
+    plusBtn.target = count++
+}
