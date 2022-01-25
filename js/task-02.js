65cda098eac96747e807a0d0a6bd00f7b066880d
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-25 07:27:14 +0300

    dssadibbsbd

--- js/task-02.js
+++ js/task-02.js
@@ -1,24 +1,3 @@
-// const ingredients = [
-//   'Potatoes',
-//   'Mushrooms',
-//   'Garlic',
-//   'Tomatos',
-//   'Herbs',
-//   'Condiments',
-// ];
-
-// const ingredientList = document.querySelector('#ingredients')
-
-// const ingredientsListItems = ingredients.map((ingredient) => {
-//   const itemLi = document.createElement('li')
-//   itemLi.textContent = ingredient
-//   itemLi.classList.add('item')
-//   console.log(itemLi)
-//   return itemLi
-// })
-
-// ulElem.append(...ingredientsListItems)
-// console.log(ingredientsListItems);
 
 const ingredients = [
   'Potatoes',
--- js/task-04.js
+++ js/task-04.js
@@ -1,41 +1,15 @@
-const minustBtn = document.querySelector('[data-action="increment"]')
-const plusBtn = document.querySelector('[data-action="decrement"]')
-const valueElem = document.getElementById('value')
-let count = 0
+const plusBtn = document.querySelector('[data-action="increment"]')
+const minusBtn = document.querySelector('[data-action="decrement"]')
+const counterValue = document.getElementById('value')
 
+plusBtn.addEventListener('click', function() {
+    counterValue.innerText = ++counterValue.innerText
+})
 
-// const plusFunction = elem => {
-//     for(const i = 0; i < count; i++) {
-//     if(document.onclick = plusBtn) {
-//         count += 1
-//         console.log(count);
-//     }
-// }
-// }
-// plusFunction()
-
-
-
-// document.onclick = event => {
-//     if(onclick.plusBtn) {
-//         console.log(minustBtn);
-//         plusFunction(minustBtn)
-//     }
-//     if(event.target.classList.contains('decrement')) {
-//         plusFunction(event.target.dataset.id)
-//     }
-// }
-
-// const plusFunction = id => {
-//     for() {
-//         valueElem++
-//     }
-// }
-
-// const minusFunction = id => {
-//     valueElem--
-// }
-
-document.onclick = event => {
-    plusBtn.target = count++
-}
+minusBtn.addEventListener('click', function() {
+    // если элемент меньше 1, не отнимать(для товаров в корзине)
+    // if( parseInt(counterValue.innerText) > 1 ) {
+    //     counterValue.innerText = --counterValue.innerText
+    // }
+    counterValue.innerText = --counterValue.innerText
+})
