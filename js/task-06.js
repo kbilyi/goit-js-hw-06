ec0cc9d4d78b13259d101bb42caa5a16d14115c9
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-27 20:54:19 +0300

    qwe

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.getElementById('validation-input')
 input.addEventListener('blur', blurInput)
 
 function blurInput(event) {
-        if() {
+        // if() {
 
-        }
+        // }
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,9 @@
+const form = document.querySelector(["submit"])
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    console.log(asdsad);
+}
