1a488696d669ec01b4d4a087b4892ac50725f9fd
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-30 15:30:34 +0300

    йцу

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,19 @@
-// const form = document.querySelector("login-form > submit")
+const form = document.querySelector("login-form > submit")
 
-// form.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit)
 
-// function onFormSubmit(event) {
-//     event.preventDefault();
+function onFormSubmit(event) {
+    event.preventDefault();
 
-//     console.log(asdsad);
-// }
+    console.log(asdsad);
+}
 
+function onFormSubmit(event) {
+    if (event.currentTarget.value === "") {
+        alert("Все поля должны быть заполнены")
+    } else {
+        // console.log(event.currentTarget.value);
+        refs.nameOutput.textContent = event.currentTarget.value
+    }
+}
 
