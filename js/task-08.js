e82f5bb30ea427fd0cd463b0358a78b3b0221b32
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-01-30 15:52:30 +0300

    sub

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,39 @@
-const form = document.querySelector("login-form > submit")
+const formSub = document.querySelector("login-form > submit")
+const formPass = document.querySelector("login-form > password")
+const formEmail = document.querySelector('login-form > email')
 
-form.addEventListener('submit', onFormSubmit)
-
-function onFormSubmit(event) {
-    event.preventDefault();
-
-    console.log(asdsad);
-}
+formSub.addEventListener('submit', onFormSubmit) 
+    
 
 function onFormSubmit(event) {
-    if (event.currentTarget.value === "") {
-        alert("Все поля должны быть заполнены")
-    } else {
-        // console.log(event.currentTarget.value);
-        refs.nameOutput.textContent = event.currentTarget.value
+    event.preventDefault()
+
+    if(formPass === '') {
+        alert('Все поля должны быть заполнены!')
+    } else if(formEmail === '') {
+        alert('Все поля должны быть заполнены!')
     }
 }
+
+
+
+
+
+
+// form.addEventListener('submit', onFormSubmit)
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     console.log(asdsad);
+// }
+
+// function onFormSubmit(event) {
+//     if (event.currentTarget.value === "") {
+//         alert("Все поля должны быть заполнены")
+//     } else {
+//         // console.log(event.currentTarget.value);
+//         refs.nameOutput.textContent = event.currentTarget.value
+//     }
+// }
 
